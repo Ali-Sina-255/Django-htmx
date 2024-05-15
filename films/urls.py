7e4408de2728521b603a04a64a6be7bc71ca3acc
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-05-15 18:06:46 +0430

    delete and search with htmx

--- films/urls.py
+++ films/urls.py
@@ -4,12 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index ,name='index'),
-    path('file-list/', views.film, name='film')
+    path('file-list/', views.film, name='film'),
 ]
 
 
 htmx_urlpatterns  =  [
-    path('check_username/', views.check_username, name='check_username')
+    path('check_username/', views.check_username, name='check_username'),
+    path('add_film/', views.add_film, name='add_film'),
+    path('delete-film/<int:pk>/', views.delete_film_view, name='delete_film'),
+    path('search/', views.search_film_view, name='search_view'),
 ]
 urlpatterns += htmx_urlpatterns
 
--- films/views.py
+++ films/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 from django.http import HttpResponse
 from. models import Film
@@ -15,8 +17,8 @@ def check_username(request):
         return HttpResponse('<div id="username-error" class="success">this username is already exists<div>')
     else:
         return HttpResponse('<div id="username-error" class="error" > this username is available<div>')
-    
-    
+
+@login_required(login_url='login')
 def film(request):
     user_film = Film.objects.filter(user=request.user)
     context = {
@@ -25,5 +27,34 @@ def film(request):
     return render(request, 'film/film.html', context)
 
 
-def add_file(request):
-    return render(request, 'add_film.html')
+@login_required(login_url='login')
+def add_film(request):
+    name = request.POST['filmname']
+    film = Film.objects.create(film=name)
+    # add the film to the user's list
+    request.user.film.add(film)
+    
+    # return template with of all user's list
+    films = request.user.film.all()
+    context = {"films":films}
+    return render(request, 'partials/film-list.html', context)
+
+
+@login_required(login_url='login')
+@require_http_methods(['DELETE'])
+def delete_film_view(request, pk):
+    # delete the user data
+    request.user.film.remove(pk)
+    film = request.user.film.all()
+    context = {"films":film}
+    return render(request, 'partials/film-list.html', context)
+
+def search_film_view(request):
+    result = request.POST.get('search')
+    result = Film.objects.filter(film__icontains=result)
+    context = {
+        "result":result
+    }
+    return render(request, 'partials/film-list.html',context)
+
+
