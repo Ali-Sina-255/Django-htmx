fd49efc29372d60392348282fb3b062441438235
dev <dev@DEV.DEV>
2024-05-19 14:52:24 +0430

    Building a Sortable Drag and Drop Interface

--- films/models.py
+++ films/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.functions import Lower
 
 
 class Film(models.Model):
-    user = models.ManyToManyField(User, related_name='film')
+    user = models.ManyToManyField(User, related_name='film', through='UserFilm')
     film = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.film
+    
+    class Meta:
+        ordering = [Lower('film')]
+
+class UserFilm(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    order = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ['order']
